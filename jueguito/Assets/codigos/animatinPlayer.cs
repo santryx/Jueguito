b99a6a0b393b538a1a6537a30e589dec80025fd5
Santy Monroy <santryx.dmc@gmail.com>
2020-03-18 20:51:56 -0500

    jueguito 1

---
+++ jueguito/Assets/codigos/animatinPlayer.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animatinPlayer : MonoBehaviour
+{
+
+    public float speedRotation = 200.0f;
+    public float speedMoving = 4.0f;
+    float yDir;
+    float xDir;
+    Animator PlayerAni;
+
+    
+    void Start()
+    {
+        PlayerAni = GetComponent<Animator>();  
+    }
+
+    
+    void Update()
+
+    {
+
+        xDir = Input.GetAxis("Horizontal");
+        yDir = Input.GetAxis("Vertical");
+
+        transform.Rotate(0, xDir* Time.deltaTime * speedRotation, 0);
+        transform.Translate(0,0,yDir* Time.deltaTime*speedMoving);
+
+        PlayerAni.SetFloat("SpeedX", xDir);
+        PlayerAni.SetFloat("SpeedY", yDir);
+
+        if(Input.GetButtonDown("Jump"))
+        {
+
+            PlayerAni.SetTrigger("Fire");
+
+        }
+
+        
+
+
+        
+
+    }
+}
